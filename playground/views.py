11cd83f1b18648e70fc9268a16f2dfbb4cb756d7
Reza Ramazani <reza_ramazani@ut.ac.ir>
2023-05-12 09:33:31 +0200

    applying and working with a bunch of query set methods

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q, F
+from store.models import Product, Customer, Collection, Order, OrderItem
 # Create your views here.
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Reza'})
+    queryset1 = Product.objects.filter(unit_price__range=(10, 30))
+    queryset2 = Product.objects.filter(
+        ~Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    # queryset33 and queryset3 have the same results.
+    queryset3 = Product.objects.filter(
+        id=F('orderitem__product_id')).distinct().order_by('title')
+    queryset33 = Product.objects.filter(id__in=OrderItem.objects.values(
+        'product_id').distinct()).order_by('title')
+    # for prod in query_set:
+    #   print(prod)
+    return render(request, 'hello.html', {'name': 'Reza', 'products': list(queryset1), 'products2': list(queryset2), 'products3': list(queryset3)})
 
 
 def tem_func(request):
-    x = 1
-    y = 2
-    return render(request, 'temp2.html')
+    queryset1 = Collection.objects.filter(featured_product_id__isnull=True)
+    queryset2 = Customer.objects.filter(email__icontains='.com')
+    queryset3 = Order.objects.filter(customer_id=1)
+    queryset4 = OrderItem.objects.filter(product__collection_id=3)
+    queryset5 = Order.objects.select_related(
+        'customer').prefetch_related('orderitem_set__product').all().order_by('-placed_at')[:5]
+    return render(request, 'temp2.html', {'collection_info': list(queryset1), 'customer_info': list(queryset2), 'order_info': list(queryset3), 'orderitem_info': list(queryset4), 'ord_info': list(queryset5)})
--- store/models.py
+++ store/models.py
@@ -10,8 +10,8 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    feature_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
@@ -20,11 +20,10 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(default='-')
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=0)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
-    colletions = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     # If you want Django use a specific name (here is products) in promotion class, use option related_name
     # promotions = models.ManyToManyField(Promotion,related_name='products')
     promotions = models.ManyToManyField(Promotion)
@@ -52,7 +51,7 @@ class Customer(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    zip = models.CharField(max_length=255, default='-')
+    # zip = models.CharField(max_length=255, default='-')
     # for one to one relationship
     # customer = models.OneToOneField(
     #   Customer, on_delete=models.CASCADE, primary_key=True)
@@ -62,25 +61,24 @@ class Address(models.Model):
 
 
 class Order(models.Model):
-    PENDING_STATUS = 'P'
-    COMPLETE_STATUS = 'C'
-    FAILD_STATUS = 'F'
-
-    PAYMENT_STATUS = [
-        (PENDING_STATUS, 'Pending'),
-        (COMPLETE_STATUS, 'Complete'),
-        (FAILD_STATUS, 'Failed'),
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
     ]
-    placed_at = models.DateTimeField(auto_now_add=True)
-    paymant_status = models.CharField(
-        max_length=1, choices=PAYMENT_STATUS, default=PENDING_STATUS)
 
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=0)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
 
@@ -91,7 +89,7 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class cartItem(models.Model):
+class CartItem(models.Model):
     quantity = models.PositiveSmallIntegerField()
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)

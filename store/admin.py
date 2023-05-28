f4a86bc54105f3a07a0a721febc0242ed1b1b5af
Reza Ramazani <reza_ramazani@ut.ac.ir>
2023-05-28 12:19:43 +0200

    adding computed columng, Selecting related objects, and overriding the base querysets

--- store/admin.py
+++ store/admin.py
@@ -1,16 +1,38 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from . import models
+from django.db.models.aggregates import Avg, Count, Max, Min, Sum, StdDev, Variance
+
 # Register your models here.
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 20
 
+    # To implement sorting based on inventory_status, use admin.display decorator
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
-admin.site.register(models.Collection)
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count('product'))
+
 # admin.site.register(models.Product)
 
 
@@ -20,3 +42,14 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 20
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'payment_status', 'customer']
+    list_editable = ['payment_status']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    # def customer_list(self, order):
+    #   return order.customer.first_name, ' ', order.customer.last_name

6d461e08b07e12699f572b7f70c895521139fbd2
Reza Ramazani <reza_ramazani@ut.ac.ir>
2023-06-10 12:23:33 +0200

    deserializing objects and data validation

--- store/views.py
+++ store/views.py
@@ -3,26 +3,34 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializer import ProductSerializer
+from .models import Product, Collection
+from .serializer import ProductSerializer, CollectionSerializer
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(
-        queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response('OKOKOK')
 
 
 @api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
+    serializer = ProductSerializer(product, context={'request': request})
     return Response(serializer.data)
 
 
 @api_view()
 def collection_detail(request, pk):
-    return Response('ok')
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)

29fca9efae80936c047bc991b71865e2e63e5eb1
Reza Ramazani <reza_ramazani@ut.ac.ir>
2023-06-12 17:18:18 +0200

    Implementing routers and modifying the destroy functions

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter, DefaultRouter
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>', views.CollectionDetail.as_view(),
-         name='collection-detail'),
-]
+router = DefaultRouter()
+# router = SimpleRouter()
+router.register('product', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+
+urlpatterns = router.urls
+# urlpatterns = [
+# path('', include(router.urls))
+# path('products/', views.ProductList.as_view()),
+# path('products/<int:pk>', views.ProductDetail.as_view()),
+# path('collections/', views.CollectionList.as_view()),
+# path('collections/<int:pk>', views.CollectionDetail.as_view(),
+#      name='collection-detail'),
+# ]
--- store/views.py
+++ store/views.py
@@ -4,11 +4,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializer import ProductSerializer, CollectionSerializer
 from django.db.models.aggregates import Count
 # Create your views here.
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'The product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
 
 
 class ProductList(ListCreateAPIView):
@@ -104,6 +118,17 @@ def product_detail(request, pk):
             return Response({'error': 'The product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(
+        products_count=Count('product')).all()
+    serializer_class = CollectionSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Collection cannot be deleted since the collection contains one or more products.'})
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionList(ListCreateAPIView):
